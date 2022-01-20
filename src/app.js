95a3ff99d4167e7c2521dc7e5a699963f802e74f
Sofia <sofiaxrk@gmail.com>
2022-01-20 12:25:26 +0000

    subiendo codigo que valida ok para eliminar despues

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+  let alert = document.querySelector(".myAlert");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function(form) {
+    form.addEventListener(
+      "submit",
+      function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          alert.classList.remove("dnone");
+          alert.classList.add("dshow");
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
